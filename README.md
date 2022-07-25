689d38957bc46316df27ae5f4c4137daecf0ef89
suneall01 <suneall01@gmail.com>
2022-07-25 11:14:39 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-2duyyq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-2duyyq)
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import App from './App';
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+createApp(App).mount('#app');
